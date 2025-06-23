e7fa949e31b27458053684368f35fc134dadba35
TOBIAS ISAAC EPSZTEIN <49194388@ort>
2025-06-23 09:06:20 -0300

    Septimo Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -84,49 +84,45 @@ public class HomeController : Controller
     {
         return View("Ganaste1");
     }
+     public IActionResult Ganaste2()
+    {
+        return View("Ganaste2");
+    }
+     public IActionResult Ganaste3()
+    {
+        return View("Ganaste3");
+    }
+     public IActionResult Ganaste4()
+    {
+        return View("Ganaste4");
+    }
+     public IActionResult Ganaste5()
+    {
+        return View("Ganaste5");
+    }
     public IActionResult Moe()
     {
         return View("moe");
-    }
-private static readonly List<string> palabrasClave = new()
-        {
-            "homero", "duff", "barney", "moe", "squishee",
-            "donuts", "flanders", "lisa", "cerveza", "springfield"
-        };
-
-        [HttpGet]
-        public IActionResult Index(int ronda = 0)
-        {
-            if (ronda < 0 || ronda >= palabrasClave.Count)
-                return RedirectToAction("Perdiste");
-
-            ViewBag.Ronda = ronda;
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Index(string FraseIngresada, int ronda)
-        {
-            if (ronda < 0 || ronda >= palabrasClave.Count)
-                return RedirectToAction("Perdiste");
-
-            var correcta = palabrasClave[ronda].ToLower();
-
-            if (FraseIngresada?.Trim().ToLower() == correcta)
-            {
-                ronda++;
-                if (ronda >= palabrasClave.Count)
-                    return RedirectToAction("Ganaste");
-
-                return RedirectToAction("Index", new { ronda });
-            }
-
-            return RedirectToAction("Perdiste");
-        }
-
-        public IActionResult Ganaste() => View();
-        public IActionResult Perdiste() => View();
+    }    
+    public IActionResult Perdiste1()
+{
+    return View("Perdiste1");
+} 
+    public IActionResult Perdiste2()
+{
+    return View("Perdiste2");
+}
+    public IActionResult Perdiste3()
+{
+    return View("Perdiste3");
+}
+    public IActionResult Perdiste4()
+{
+    return View("Perdiste4");
+}
+    public IActionResult Perdiste5()
+{
+    return View("Perdiste5");
 }
 
-
-
+}
